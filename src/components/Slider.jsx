697b1192ce13b4d09249bbcf6d1946563a85c58d
Kawsar Ahmed Fahad <kafahad1999@gmail.com>
2022-09-03 02:39:37 +0600

    complete slider

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { showCaseData } from '../data/showCaseData';
 import arrow from '../assets/images/icon-arrow.svg';
 import left from '../assets//images/icon-angle-left.svg';
 import right from '../assets//images/icon-angle-right.svg';
 
 export const Slider = () => {
-  // const [items] = useState(showCaseData);
+  const [slideIndex, setSlideIndex] = useState(1);
+
+  function nextSlide() {
+    if (slideIndex !== showCaseData.length) {
+      setSlideIndex(slideIndex + 1);
+    } else if (slideIndex === showCaseData.length) {
+      setSlideIndex(1);
+    }
+  }
+
+  function previousSlide() {
+    if (slideIndex !== 1) {
+      setSlideIndex(slideIndex - 1);
+    } else if (slideIndex === 1) {
+      setSlideIndex(showCaseData.length);
+    }
+  }
+
   return (
     <>
       <section>
-        {showCaseData.map((data) => (
+        {showCaseData.map((data, index) => (
           <article
             key={data.id}
-            className="grid grid-cols-1 lg:grid-cols-2 lg:place-items-center"
+            className={
+              slideIndex === index + 1
+                ? 'grid grid-cols-1 lg:grid-cols-2 lg:place-items-center'
+                : 'hidden'
+            }
           >
             <div>
               <picture>
@@ -25,7 +46,7 @@ export const Slider = () => {
               <h1 className="text-slate-900 text-3xl lg:text-5xl">
                 {data.title}
               </h1>
-              <p className="text-slate-900 opacity-75 my-6">{data.desc}</p>
+              <p className="text-slate-900 opacity-75 my-6 px-2">{data.desc}</p>
 
               <button
                 className="flex items-center gap-4 uppercase hover:opacity-75"
@@ -39,7 +60,7 @@ export const Slider = () => {
               <ul className="absolute -bottom-11 left-0 flex">
                 <li>
                   <button
-                    // onClick={previousSlide}
+                    onClick={previousSlide}
                     className="bg-black hover:bg-neutral-700 transition-all duration-200"
                   >
                     <img src={left} alt="left" className="p-6" />
@@ -47,7 +68,7 @@ export const Slider = () => {
                 </li>
                 <li>
                   <button
-                    // onClick={nextSlide}
+                    onClick={nextSlide}
                     className="bg-black hover:bg-neutral-700 transition-all duration-200"
                   >
                     <img src={right} alt="right" className="p-6" />
